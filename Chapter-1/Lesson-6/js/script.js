3fd57d6d0dd0b9d03ac5729bba86063b00173015
your_user_namer <urvashigolu29@gmail.com>
2025-03-29 21:23:49 +0530

    added objects in Chapter-1/Lesson-6

--- Chapter-1/Lesson-6/js/script.js
+++ Chapter-1/Lesson-6/js/script.js
@@ -54,3 +54,138 @@ console.log(person);
 // find the highest and lowest value in array -loops
 // display first 3 elements in an array in console
 // remove 4th (index) element and add 2 element there
+
+// Objects
+// this keyword (in function)
+// set
+// map
+// 2D Array
+// JSON (after functions)
+
+// how many way we can create object
+// 1. object literal
+var person = {
+  firstName: "alina",
+  lastName: "joe",
+  age: 20,
+  details: function () {},
+};
+
+// 2. using Object.create()
+var personDetails = Object.create(person);
+personDetails.contactNumber = 89899898;
+personDetails.age = 40;
+console.log(personDetails.firstName);
+console.log(person.details());
+
+// 3. new keyword (class & propotype)
+// var student = new University();
+
+//accessing object properties
+console.log(person.firstName);
+console.log(person["firstName"]);
+
+// Object methods
+console.log(Object.keys(person)); //return the array with object property
+console.log(Object.values(person)); //return the array with object values
+console.log(Object.entries(person));
+// console.log(Object.freeze(person));/can't modify
+
+// to update a object
+person.lastName = "alaya";
+console.log(person);
+
+// to add new property
+person.emailAddress = "admin@gmail.com";
+console.log(person);
+
+//delete
+delete person.age;
+console.log(person);
+
+// for...in (2015) iteration over objects
+for (var x in person) {
+  console.log(x); //property
+  console.log(person[x]); //value
+}
+
+// Set: built-in object | store data in the form arrays | does't allow duplicate value
+
+// create & add data
+var setValue = new Set(["pink", "red", "orange", "orange", "white", "yellow"]);
+console.log(setValue);
+
+// set methods
+var mySet = new Set();
+mySet.add(1);
+mySet.add(2);
+mySet.add(3);
+mySet.add(4);
+console.log(mySet);
+
+mySet.delete(4);
+console.log(mySet);
+
+// mySet.clear();
+// console.log(mySet);
+
+console.log(mySet.has(2)); //if element exits return true
+
+console.log(mySet.size); //return size of the set
+
+// iterate
+for (var color of setValue) {
+  console.log(color);
+}
+
+// set to array
+const colorArray = Array.from(setValue);
+console.log(colorArray);
+
+// Map: built-in object | store data in key value pair | store key/property of any datatype
+
+// create
+var myMap = new Map();
+
+// Map methods
+myMap.set("name", "alex");
+myMap.set("last", "joe");
+myMap.set("age", "24");
+myMap.set("1", "hello");
+console.log(myMap);
+
+myMap.delete("name");
+console.log(myMap);
+
+// myMap.clear();
+// console.log(myMap);
+
+console.log(myMap.size);
+
+console.log(myMap.has("name"));
+
+// convert map to array
+var arrayMap = Array.from(myMap);
+console.log(arrayMap);
+
+// iteration through for..of
+
+// mutidimensional array: [[], [], []]
+
+// 2D Array
+var arr = [
+  ["apple", "orange"], //0th
+  ["grape", "pear"], //1st
+  ["pineapple", "mango"], //2nd
+];
+
+console.log(arr[0][0]); //apple
+
+arr.push(["mango", "orange"]);
+
+// iteration
+for (var fruit of arr) {
+  for (var ele of fruit) {
+    console.log(ele);
+  }
+}
